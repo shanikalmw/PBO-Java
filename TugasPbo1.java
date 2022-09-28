3e20bc3d534663460fa6196415fbcd26afb8414a
shanikalmw <114599112+shanikalmw@users.noreply.github.com>
2022-09-28 22:31:20 +0700

    Add files via upload

---
+++ TugasPbo1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shanika
+ */
+public class TugasPbo1 {class TugasPbo1
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+        System.out.println("\"Belajar JAVA\"");
+        System.out.println("\"Sangat mudah sekali\"");
+        System.out.println("\"dan sangat menyenangkan\"");
+    
+}
